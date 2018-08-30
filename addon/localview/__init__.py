c283c91d11693fc77ca10ad41d8f48a16fcadcbf
solpie <solpie.net@gmail.com>
2018-08-30 10:31:08 +0800

    local view

--- addon/localview/__init__.py
+++ addon/localview/__init__.py
@@ -5,7 +5,7 @@ bl_info = {
     "name": "local view",
     "author": "SolPie",
     "version": (0, 1),
-    "blender": (2, 80, 5),
+    "blender": (2, 80, 0),
     "location": "View3D > View",
     "description": "",
     "warning": "",
@@ -14,6 +14,31 @@ bl_info = {
 import localview
 localview.restore_obj = {}
 localview.localview_obj = {}
+
+localview.isLocalView = False
+
+
+def restoreView():
+    # for new obj
+    bpy.ops.object.select_all(action='SELECT')
+    for obj in bpy.context.selected_objects:
+        localview.localview_obj[obj.name] = True
+
+    bpy.ops.object.hide_view_clear()
+    for obj in bpy.data.objects:
+        if obj.name in localview.restore_obj or obj.name in localview.localview_obj:
+            obj.select_set(action='SELECT')
+        else:
+            obj.select_set(action='DESELECT')
+    bpy.ops.object.hide_view_set(unselected=True)
+
+    for obj in bpy.data.objects:
+        if obj.name in localview.localview_obj:
+            obj.select_set(action='SELECT')
+        else:
+            obj.select_set(action='DESELECT')
+    bpy.ops.object.select_all(action='DESELECT')
+    pass
 
 
 class LocalViewRestore(bpy.types.Operator):
@@ -26,25 +51,7 @@ class LocalViewRestore(bpy.types.Operator):
         return True
 
     def execute(self, context):
-        # for new obj
-        bpy.ops.object.select_all(action='SELECT')
-        for obj in bpy.context.selected_objects:
-            localview.localview_obj[obj.name] = True
-
-        bpy.ops.object.hide_view_clear()
-        for obj in bpy.data.objects:
-            if obj.name in localview.restore_obj or obj.name in localview.localview_obj:
-                obj.select_set(action='SELECT')
-            else:
-                obj.select_set(action='DESELECT')
-        bpy.ops.object.hide_view_set(unselected=True)
-
-        for obj in bpy.data.objects:
-            if obj.name in localview.localview_obj:
-                obj.select_set(action='SELECT')
-            else:
-                obj.select_set(action='DESELECT')
-        bpy.ops.object.select_all(action='DESELECT')
+        restoreView()
         return {'FINISHED'}
 
 
@@ -58,36 +65,48 @@ class LocalViewEnable(bpy.types.Operator):
         return True
 
     def execute(self, context):
-        restore_obj = {}
-        localview_obj = {}
-        for obj in bpy.context.selected_objects:
-            # restore_obj[obj.name] = True
-            localview_obj[obj.name] = True
+        if not localview.isLocalView:
+            if len(bpy.context.selected_objects) < 1:
+                return {'FINISHED'}
+            localview.isLocalView = True
+             ##
+            restore_obj = {}
+            localview_obj = {}
+            for obj in bpy.context.selected_objects:
+                # restore_obj[obj.name] = True
+                localview_obj[obj.name] = True
 
-        bpy.ops.object.select_all(action='INVERT')
+            bpy.ops.object.select_all(action='INVERT')
 
-        for obj in bpy.context.selected_objects:
-            restore_obj[obj.name] = True
-            print('restore', obj.name)
+            for obj in bpy.context.selected_objects:
+                restore_obj[obj.name] = True
+                print('restore', obj.name)
 
-        localview.restore_obj = restore_obj
-        localview.localview_obj = localview_obj
-        bpy.ops.object.hide_view_set(unselected=False)
+            localview.restore_obj = restore_obj
+            localview.localview_obj = localview_obj
+            bpy.ops.object.hide_view_set(unselected=False)
 
-        print('restore_obj', restore_obj)
+            print('restore_obj', restore_obj)
+        else:
+            localview.isLocalView = False
+            restoreView()
         return {'FINISHED'}
 
 
 def menu_func(self, context):
     self.layout.operator(LocalViewEnable.bl_idname, text="local view")
     self.layout.operator(LocalViewRestore.bl_idname, text="local view restore")
+classes = (LocalViewEnable, LocalViewRestore)
 
 
 def register():
-    bpy.utils.register_module(__name__)
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
     bpy.types.VIEW3D_MT_view.append(menu_func)
 
 
 def unregister():
-    bpy.utils.unregister_module(__name__)
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
     bpy.types.VIEW3D_MT_view.remove(menu_func)
